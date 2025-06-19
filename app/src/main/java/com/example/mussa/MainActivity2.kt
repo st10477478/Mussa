198deb8b6624221ef7547b9b31371a4ce70d810b
st10477478 <st10477478@rcconnect.edu.za>
2025-06-19 11:48:20 +0200

    repository creation

--- app/src/main/java/com/example/mussa/MainActivity2.kt
+++ app/src/main/java/com/example/mussa/MainActivity2.kt
@@ -1,45 +1,39 @@
 package com.example.mussa
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mussa.ui.theme.MussaTheme
+import kotlinx.serialization.encoding.AbstractEncoder
 
-class MainActivity : ComponentActivity() {
+class MainActivity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Column {
-                Text(text = "Welcome to Musa!")
+            Button(onClick = {
+                Text(text = "Beyonce")
+            }) { }
+        }
 
-                Button(onClick = {
-                    Text(text = "Beyonce")
 
-                }
 
-                ) { }
+        
 
-                }
-                }
 
-            }
-            }
+
+
 
 
 
